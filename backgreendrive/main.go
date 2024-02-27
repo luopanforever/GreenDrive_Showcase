cfbf38810eb3920dabd04e2684450a96ac2550b0
luopanforever <979568276@qq.com>
2024-02-27 21:03:38 +0800

    init greendrive_showcase

---
+++ backgreendrive/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/luopanforever/backgreendrive/middlewares"
+)
+
+func main() {
+	r := gin.Default()
+	r.Use(middlewares.CORSMiddleware())
+
+	r.PUT("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hello world",
+		})
+	})
+
+	r.Run() // 默认在0.0.0.0:8080启动服务
+}
