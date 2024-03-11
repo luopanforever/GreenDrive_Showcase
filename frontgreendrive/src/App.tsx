43fba879f03bc1b6e2be888530c92dd4fe2378e1
luopanforever <979568276@qq.com>
2024-03-12 03:02:23 +0800

    change noting...

--- frontgreendrive/src/App.tsx
+++ frontgreendrive/src/App.tsx
@@ -1,24 +1,63 @@
-// import React, { useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import axios from 'axios';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 function App() {
-  // const [ state, setstate ] = useState()
-  const fetchData = async () => {
-    try {
-      const response = await axios.get('/car/testCar01.glb');  // /api/test /car/testCar01.glb
-      console.log(response.data); // 这会在控制台中输出后端返回的数据
-      // setstate(response.data)
-    } catch (error) {
-      console.error('There was an error!', error);
-    }
+  const mountRef = useRef<HTMLDivElement>(null);
+
+  const loadGLBModel = async (url: string) => {
+    const loader = new GLTFLoader();
+
+    const glb = await axios.get(url, { responseType: 'blob' });
+    const glbUrl = URL.createObjectURL(glb.data);
+
+    loader.load(
+      glbUrl,
+      (gltf) => {
+        console.log(gltf); // 这里可以访问到GLTF模型的数据
+
+        // 你可以在这里处理gltf.scene或gltf对象，提取需要的数据并以你想要的格式输出或使用
+        // 例如，打印出scenes数组
+        console.log('Scenes:', gltf.scenes);
+
+        const scene = new THREE.Scene();
+        scene.add(gltf.scene);
+
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        camera.position.z = 5;
+
+        const renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        mountRef.current?.appendChild(renderer.domElement);
+
+        const animate = () => {
+          requestAnimationFrame(animate);
+          renderer.render(scene, camera);
+        };
+
+        animate();
+      },
+      undefined,
+      (error) => {
+        console.error(error);
+      }
+    );
   };
+
+  useEffect(() => {
+    return () => {
+      if (mountRef.current?.firstChild) {
+        mountRef.current.removeChild(mountRef.current.firstChild);
+      }
+    };
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        {/* 其他内容 */}
-        {/* {state} */}
-        <button onClick={() => fetchData()}>请求数据</button>
+        <div ref={mountRef}></div>
+        <button onClick={() => loadGLBModel('/car/car01/car2.gltf')}>加载3D模型</button>
       </header>
     </div>
   );
