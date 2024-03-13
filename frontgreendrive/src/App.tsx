d6014f5adfa9cd96b21ab4c68c7ce0904f1f0fc5
luopanforever <979568276@qq.com>
2024-03-13 16:10:30 +0800

    cange get uri

--- frontgreendrive/src/App.tsx
+++ frontgreendrive/src/App.tsx
@@ -5,7 +5,7 @@ const App: React.FC = () => {
     <>
       <ShowModel
         style={{ width: 600, height: 600 }}
-        url='http://localhost:8080/car/car01/car01.gltf'
+        url='http://localhost:8080/car/show/car1/car1.gltf'
       />
       {/* <ShowModel style={{width:600,height:600}} url='https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Duck/glTF/Duck.gltf'/> */}
     </>
